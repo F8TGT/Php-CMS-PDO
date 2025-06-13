4e8fdc698863224f30caa7bd98d32a54b0799254
Edward <78192184+F8TGT@users.noreply.github.com>
2025-06-13 08:31:05 -0600

    → Set up the login, register, about and contact pages.

--- partials/header.php
+++ partials/header.php
@@ -9,5 +9,6 @@
     <title>CMS PDO System</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
