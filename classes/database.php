7270fb0046bac0f3244d6abe0fc42c1856d29ccf
Edward <78192184+F8TGT@users.noreply.github.com>
2025-06-20 13:15:28 -0600

    → Created the config file.

---
+++ classes/database.php
@@ -0,0 +1,12 @@
+<?php
+
+class database
+{
+    private $host = DB_HOST;
+    private $db_name = DB_NAME;
+    private $username = DB_USER;
+
+    private $password = DB_PASS;
+
+    public $conn;
+}
--- init.php
+++ init.php
@@ -3,6 +3,12 @@
 // START SESSION
 session_start();
 
+// INCLUDE THE MAIN CONFIGURATION FILE
+require_once "config/config.php";
+
+// LOAD DATABASE
+require_once "classes/database.php";
+
 // INCLUDE HELPER FUNCTIONS
 require_once "helpers.php";
 
