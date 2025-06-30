794b3159318fd2348d96605e55f68104879b9022
Edward <78192184+F8TGT@users.noreply.github.com>
2025-06-30 10:01:13 -0600

    → Added a functionality to log in with a user.

--- login.php
+++ login.php
@@ -3,6 +3,19 @@
 include "partials/header.php";
 include "partials/navbar.php";
 include "partials/hero.php";
+
+if (isPostRequest()) {
+    $email = getPostData('email');
+    $password = getPostData('password');
+
+    $user = new User();
+
+    if ($user->login($email, $password)) {
+        redirect("admin.php");
+    } else {
+        echo "Login Failed";
+    }
+}
 ?>
 
 <!-- Main Content -->
@@ -10,14 +23,14 @@ include "partials/hero.php";
     <h2 class="text-center mb-4">Login</h2>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <form action="dashboard.php" method="post">
+            <form method="post">
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address *</label>
-                    <input type="email" class="form-control" id="email" required>
+                    <input name="email" type="email" class="form-control" id="email" required>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password *</label>
-                    <input type="password" class="form-control" id="password" required>
+                    <input name="password" type="password" class="form-control" id="password" required>
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Login</button>
             </form>
--- partials/admin/header.php
+++ partials/admin/header.php
@@ -1,6 +1,6 @@
 <?php
+
 include __DIR__."/../../init.php";
-echo base_url("cms-pdo");
 ?>
 
 <!DOCTYPE html>
